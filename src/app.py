bda063f245e4d57fc1914812a540f11b8f6cf7bb
bct9321 <bct9321@rit.edu>
2023-07-26 02:46:07 -0400

    Update app.py
    removed flask in favor of tkinter

--- src/app.py
+++ src/app.py
@@ -1,20 +1,25 @@
-# Import necessary modules
-from flask import Flask, render_template, request
-from animend import process_directories_batch  # Import function from animend.py
+import tkinter as tk
+from tkinter import messagebox
+from animend import process_directories_batch
 
-# Create the Flask application
-app = Flask(__name__)
-
-# Route for the home page
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-# Route to process the directories and display results
-@app.route('/process', methods=['POST'])
 def process_directories():
-    process_directories_batch()  # Call the function from animend.py
-    return render_template('result.html')
+    try:
+        process_directories_batch()
+        messagebox.showinfo("Success", "Processing completed!")
+    except Exception as e:
+        messagebox.showerror("Error", f"An error occurred: {str(e)}")
 
-if __name__ == "__main__":
-    app.run(debug=True)
+# Create the main application window
+root = tk.Tk()
+root.title("AniMend")
+
+# Create a label to display instructions
+instruction_label = tk.Label(root, text="Click the button to process anime directories:")
+instruction_label.pack(pady=10)
+
+# Create a button to trigger the processing
+process_button = tk.Button(root, text="Process Directories", command=process_directories)
+process_button.pack(pady=10)
+
+# Start the Tkinter event loop
+root.mainloop()
